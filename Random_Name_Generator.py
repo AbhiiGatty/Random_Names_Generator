107be489197d52bf045e9d2f6048b81a85f85ead
Hitoshirenu <abhiigatty@gmail.com>
2018-10-02 19:22:53 +0530

    Created a better cli and logic design to select random names and added a larger first and last name list

--- Random_Name_Generator.py
+++ Random_Name_Generator.py
@@ -1,25 +1,36 @@
-from random import choice
-import csv
+#! /usr/bin/env python3
+from pathlib import Path
+import argparse
+import random
 
+""" Generate a random line from a file without reading the entire file
+    Inspired from: https://stackoverflow.com/a/35579149 """
+def random_line(file_path):
+    line_num = 0
+    selected_line = ''
+    with file_path.open(mode="r") as fp:
+        while 1:
+            line = fp.readline()
+            if not line: break
+            line_num += 1
+            if random.uniform(0, line_num) < 1:
+                selected_line = line
+    return selected_line.strip()
 
-def random_name_generator(first, second, x):
-    """
-        Generates random names.
-        Arguments:
-         - list of first names
-         - list of last names
-         - number of random names
-    """
-    names = []
-    for i in range(x):
-        names.append("{0} {1}".format(choice(first), choice(second)))
-    return set(names)
+""" Output a given number of random names """
+def random_names(number_of_names = 1):
+    first_names_file = Path("CSV_Database_Of_First_And_Last_Names/CSV_Database_of_First_Names.csv") 
+    last_names_file = Path("CSV_Database_Of_First_And_Last_Names/CSV_Database_of_Last_Names.csv")
+    if first_names_file.is_file() and last_names_file.is_file(): # Check if file exists
+        for i in range(number_of_names):
+            random_first_name = random_line(first_names_file)
+            random_last_name = random_line(last_names_file)
+            print(f'{random_first_name} {random_last_name}')
 
-file=open("First_Names.txt",'r+')
+if __name__ == '__main__':
+    # Accept command line argument for number of names required where default is 1
+    parser = argparse.ArgumentParser(description='Generate Random Names')
+    parser.add_argument('num', nargs='?', type=int, default=1)
+    args = parser.parse_args()
+    random_names(args.num)
 
-file.seek(0)
-
-first_names=file.readlines()
-last_names = ["Smith", "Jones", "Brighton", "Taylor"]
-names = random_name_generator(first_names, last_names, 5)
-print('\n'.join(names))
